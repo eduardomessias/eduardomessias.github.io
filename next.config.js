8070e7b5e330a7f765f4d73bd35ae96401c8a6ad
Eduardo Messias de Andrade e Oliveira <eduardo.messias@outlook.com>
2022-11-15 11:59:19 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
 }
 
-module.exports = nextConfig
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+}
