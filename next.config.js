9c7995c1fcffc3543ce23f37d6302478a0d84130
Eduardo Messias de Andrade e Oliveira <eduardo.messias@outlook.com>
2022-11-15 13:09:37 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,18 @@
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-let assetPrefix = `/${repo}/`
+/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
-  assetPrefix: assetPrefix,
+  /**
+   * Tell Next.js where the `public` folder is.
+   * Replace `nextjs-github-pages` with your Github repo project name.
+   */
+  assetPrefix: isProd ? "/nextjs-github-pages/" : "",
+  /**
+   * Disable server-based image optimization.
+   *
+   * @see https://nextjs.org/blog/next-12-3#disable-image-optimization-stable
+   */
   images: {
-    loader: 'imgix',
-    path: 'eduardomessias.imgix.net',
+    unoptimized: true,
   },
-}
+};
