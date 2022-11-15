0290df74bb8756eaac2123f3bd6886803d47e18e
Eduardo Messias de Andrade e Oliveira <eduardo.messias@outlook.com>
2022-11-15 12:16:20 +0000

    pure next

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = '/'
-
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
 }
 
-module.exports = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-}
+module.exports = nextConfig
