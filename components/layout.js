60e7246a3a9ff03e646f8854454dd26d41d1b10d
Eduardo Messias de Andrade e Oliveira <eduardo.messias@outlook.com>
2022-11-17 18:20:39 +0000

    playing around with design

---
+++ components/layout.js
@@ -0,0 +1,4 @@
+import styles from './layout.module.css'
+
+export default ({children}) =>
+    <main className={styles.mainContainer}>{children}</main>
---
+++ components/search.js
@@ -0,0 +1,4 @@
+import styles from './search.module.css'
+
+export default () =>
+    <input className={styles.searchInput} type='search' placeholder='search something...' />
